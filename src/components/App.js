38a3cf6e513cdafebb329218d60d04bb05b5a240
codeme <cosimo@demedici.me>
2019-09-21 23:07:24 +0200

    added Redux selectors

--- src/components/App.js
+++ src/components/App.js
@@ -1,117 +1,63 @@
-import React, { Component } from 'react';
-import './App.css';
-import Web3 from 'web3'
-import Token from '../abis/Token.json'
+import React, { Component } from 'react'
+import './App.css'
+import Navbar from './Navbar'
+import Content from './Content'
+import { connect } from 'react-redux'
+import {
+  loadWeb3,
+  loadAccount,
+  loadToken,
+  loadExchange
+} from '../store/interactions'
+import { contractsLoadedSelector, accountSelector } from '../store/selectors'
 
 class App extends Component {
   componentWillMount() {
-    this.loadBlockchainData()
+    this.loadBlockchainData(this.props.dispatch)
   }
 
-  async loadBlockchainData() {
+  async loadBlockchainData(dispatch) {
 
-    const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545')
-    const block = await web3.eth.getBlockNumber()
-    console.log(block)
-    const network = await web3.eth.net.getNetworkType()
-    console.log(network)
+    const web3 = loadWeb3(dispatch)
+    await web3.eth.net.getNetworkType()
     const networkId = await web3.eth.net.getId()
-    console.log(networkId)
-    await web3.eth.getAccounts().then(console.log);
-    const token = web3.eth.Contract(Token.abi, Token.networks[networkId].address)
-    const totalSupply = await token.methods.totalSupply().call()
-    console.log("totalSupply", totalSupply)
+    await loadAccount(web3, dispatch)
+
+    const token = await loadToken(web3, networkId, dispatch)
+
+    if(!token) {
+      window.alert('Token smart contract not detected on the current network. Please select another network with Metamask.')
+      return
+    }
+
+    const exchange = await loadExchange(web3, networkId, dispatch)
+
+    if(!exchange) {
+      window.alert('Exchange smart contract not detected on the current network. Please select another network with Metamask.')
+      return
+    }
   }
 
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-          <a className="navbar-brand" href="/#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="/#">Link 1</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/#">Link 2</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/#">Link 3</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <div className="content">
-          <div className="vertical-split">
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-          </div>
-          <div className="vertical">
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-          </div>
-          <div className="vertical-split">
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-          </div>
-          <div className="vertical">
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Navbar />
+        { this.props.contractsLoaded ? <Content /> : <div className="content"></div> }
+
       </div>
-    );
+    )
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    account: accountSelector(state),
+    contractsLoaded: contractsLoadedSelector(state)
+  }
+}
+
+export default connect(mapStateToProps)(App)
+
+
+
+
