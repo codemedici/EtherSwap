e069b2167f1a5a236541380e12a2ab2422cf0740
codeme <cosimo@demedici.me>
2019-10-06 13:47:45 +0200

    more changes hoping to fix the fact that the app fails to call getAccounts() via the web3 provider

--- src/components/App-copy.js
+++ src/components/App-copy.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import './CandleStickChart.css'
 import Navbar from './Navbar'
 import Content from './Content'
 import { connect } from 'react-redux'
@@ -10,6 +11,15 @@ import {
   loadExchange
 } from '../store/interactions'
 import { contractsLoadedSelector } from '../store/selectors'
+// imports for candlestick chart
+import * as d3 from 'd3';
+import * as d3queue from 'd3-queue';
+import * as topojson from 'topojson';
+/* Add these to package.json
+    "d3": "3.5.17",
+    "d3-queue": "3.0.7",
+    "topojson": "3.0.2"
+*/
 
 class App extends Component {
   componentWillMount() {
@@ -18,11 +28,9 @@ class App extends Component {
 
   async loadBlockchainData(dispatch) {
     const web3 = loadWeb3(dispatch)
-    console.log(web3)
     await web3.eth.net.getNetworkType()
     const networkId = await web3.eth.net.getId()
-    const account = await loadAccount(web3, dispatch)
-    console.log(account)
+    await loadAccount(web3, dispatch)
     const token = await loadToken(web3, networkId, dispatch)
     if(!token) {
       window.alert('Token smart contract not detected on the current network. Please select another network with Metamask.')
@@ -38,6 +46,11 @@ class App extends Component {
   render() {
     return (
       <div>
+        <script src="cschart.js"></script>
+        <script src="csbars.js"></script>
+        <script src="csheader.js"></script>
+        <script src="csdataprep.js"></script>
+        <script src="csmain.js"></script>
         <Navbar />
         { this.props.contractsLoaded ? <Content /> : <div className="content"></div> }
       </div>
--- src/store/interactions.js
+++ src/store/interactions.js
@@ -32,6 +32,7 @@ export const loadWeb3 = (dispatch) => {
 }
 
 export const loadAccount = async (web3, dispatch) => {
+  // TODO add warning message if no accounts found
   const accounts = await web3.eth.getAccounts()
   const account = accounts[0]
   dispatch(web3AccountLoaded(account))
